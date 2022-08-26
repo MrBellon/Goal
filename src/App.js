691ec57ec572bbe3b1a676135f4681f480d5ce9c
MrBellon <104333083+MrBellon@users.noreply.github.com>
2022-08-27 04:12:27 +0900

    drilling 해결 및 완성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useReducer, useRef } from 'react';
+import React, { useCallback, useEffect, useMemo, useReducer, useRef } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -28,9 +28,11 @@ const reducer = (state, action)=>{
   }
 }
 
-const App = () => {
-  // const [data, setData] = useState([]);
+export const DiaryStateContext = React.createContext();
 
+export const DiaryDispatchContext = React.createContext();
+
+const App = () => {
   const [data, dispatch] = useReducer(reducer, [])
 
   const dataId = useRef(0);
@@ -72,6 +74,10 @@ const App = () => {
     dispatch({type: 'EDIT', targetId, newContent})
   }, []);
 
+  const memoizeDispatches = useMemo(() => {
+    return {onCreate, onRemove, onEdit}
+  }, [])
+
   const getDiaryAnalysis = useMemo(
     () => {
     const goodCount = data.filter((it) => it.emotion >= 3).length;
@@ -82,14 +88,20 @@ const App = () => {
 
   const {goodCount, badCount, goodRatio} = getDiaryAnalysis;
 
-  return <div className='App'>
-    <DiaryEditor onCreate={onCreate} />
-    <div>전체 일기 : {data.length}</div>
-    <div>기분 좋은 일기 개수 : {goodCount}</div>
-    <div>기분 나쁜 일기 개수 : {badCount}</div>
-    <div>기분 좋은 일기 비율 : {goodRatio}</div>
-    <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList = {data} />
-  </div>
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={memoizeDispatches}>
+      <div className='App'>
+        <DiaryEditor />
+        <div>전체 일기 : {data.length}</div>
+        <div>기분 좋은 일기 개수 : {goodCount}</div>
+        <div>기분 나쁜 일기 개수 : {badCount}</div>
+        <div>기분 좋은 일기 비율 : {goodRatio}</div>
+        <DiaryList />
+      </div>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
+  );
 }
 
 export default App;
